cfc8f951500cb807df521b940061a2ef01267075
default <mjbarrett@mcw.edu>
2024-06-25 11:47:15 +0000

    add environmental variables

--- src/omero_scoper/__main__.py
+++ src/omero_scoper/__main__.py
@@ -1,9 +1,22 @@
+import os
 import argparse
 from flask import Flask, jsonify
 from omero.gateway import BlitzGateway
-from omero_scoper.scopers import OmeroBaseScoper
 from omero_scoper.scopers.OmeroSlideScoper import OmeroSlideScoper
 from omero_scoper.scopers.OmeroSubjectScoper import OmeroSubjectScoper
+
+class EnvDefault(argparse.Action):
+    def __init__(self, envvar, required=True, default=None, **kwargs):
+        if envvar:
+            if envvar in os.environ:
+                default = os.environ[envvar]
+        if required and default:
+            required = False
+        super(EnvDefault, self).__init__(default=default, required=required, 
+                                         **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, values)
 
 class OmeroScoperApp(Flask):
     def __init__(self, import_name, conn, group_id, exclusive_tagset_ids, scoper_type):
@@ -37,15 +50,24 @@ def main(**kwargs):
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Run OmeroBaseScoper as an HTTP endpoint.")
-    parser.add_argument('--username', required=True, help='OMERO server username')
-    parser.add_argument('--password', required=True, help='OMERO server password')
-    parser.add_argument('--host', required=True, help='OMERO server host')
-    parser.add_argument('--port', type=int, default=None, help='OMERO server port')
-    parser.add_argument('--secure', action='store_true', help='Use secure OMERO server connection')
-    parser.add_argument('--group_id', type=int, default=-1, help='Group ID to scope to')
-    parser.add_argument('--exclusive_tagset_ids', nargs='+', type=int, default=[], help='List of exclusive tagset IDs')
-    parser.add_argument('--scoper_type', required=True, choices=['subject', 'slide'], help='Type of scoper to use')
-    parser.add_argument('--http_port', type=int, default=8080, help='Port to run the HTTP server on')
+    parser.add_argument('--username', required=True, help='OMERO server username',
+                        action=EnvDefault, envvar='OMERO_USERNAME')
+    parser.add_argument('--password', required=True, help='OMERO server password',
+                        action=EnvDefault, envvar='OMERO_PASSWORD')
+    parser.add_argument('--host', required=True, help='OMERO server host',
+                        action=EnvDefault, envvar='OMERO_HOST')
+    parser.add_argument('--port', type=int, default=None, help='OMERO server port',
+                        action=EnvDefault, envvar='OMERO_PORT')
+    parser.add_argument('--secure', action='store_true', help='Use secure OMERO server connection',
+                        action=EnvDefault, envvar='OMERO_SECURE')
+    parser.add_argument('--group_id', type=int, default=-1, help='Group ID to scope to',
+                        action=EnvDefault, envvar='OMERO_GROUP_ID')
+    parser.add_argument('--exclusive_tagset_ids', nargs='+', type=int, default=[], help='List of exclusive tagset IDs',
+                        action=EnvDefault, envvar='OMERO_TAGSET_IDS')
+    parser.add_argument('--scoper_type', required=True, choices=['subject', 'slide'], help='Type of scoper to use. Can be "subject" or "slide"',
+                        action=EnvDefault, envvar='SCOPER_TYPE')
+    parser.add_argument('--http_port', type=int, default=8080, help='Port to run the HTTP server on',
+                        action=EnvDefault, envvar='HTTP_PORT')
 
     args = parser.parse_args()
     main(**args)
